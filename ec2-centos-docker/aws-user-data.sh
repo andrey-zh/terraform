b3e05ba8e8fd0a670e578b043ae102db4d31b643
andrey-zh <andrew.z@onmail.com>
2022-06-18 18:53:28 +0100

    modified user data shell script

--- ec2-centos-docker/aws-user-data.sh
+++ ec2-centos-docker/aws-user-data.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
-# Install httpd and set it to run at startup
-sudo yum update -y
-sudo yum install -y httpd
-sudo systemctl start httpd
-sudo systemctl enable httpd
-echo "<h1>AWS VM deployed using Terraform</h1>" | sudo tee /var/www/html/index.html
-
 # Install Docker
 sudo yum install -y docker
 sudo service docker start
 sudo usermod -a -G docker ec2-user
 sudo chkconfig docker on
 sudo service docker restart
-
